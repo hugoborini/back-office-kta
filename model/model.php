db9425546647417e8d1bb4f255f1ba8293b88878
hugo borini <hugo.borini@hetic.net>
2020-06-11 17:13:44 +0200

    :lipstick: {add} ui soft

--- model/model.php
+++ model/model.php
@@ -1,6 +1,6 @@
 <?php
 function dbConnect() {
-    try { $bdd = new PDO('mysql:host=localhost;dbname=catacombes;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    try { $bdd = new PDO('mysql:host=localhost;dbname=catacombes;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     }
     catch (Exception  $e) {
         $retour["sucess"] = false;
--- views/admin.php
+++ views/admin.php
@@ -11,19 +11,18 @@ $all_room = showAllRoom();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/css/styles.css">
     <title>Catacombes | Admin</title>
+    <link rel="stylesheet" href="node_modules/font-awesome/css/font-awesome.css">
 </head>
 <body>
-    <h1>Catacombes</h1>
-    <div class='cards'>
+    <header>Catacombes</header>
+    <div class="list-container">
+    <ul class="list">
     <?php while($data_room = $all_room->fetch()){ ?>
-<div class='card'>
-    <div class='card__name'> <?=$data_room['name']?></div>
-    <div class='card__description'><?=$data_room['description']?> </div>
-    <div class='card__path_img'><?=$data_room['path_img']?> </div>
-    <a href="admin/<?= $data_room['id_room']?>">Voir plus</a> <a href="delete/<?= $data_room['id_room']?>">Delete room</a>
-</div>
+
+    <li class="list__elem"><?=$data_room['name']?> <div><a href="delete/<?= $data_room['id_room']?>"><i class="fa fa-trash del"></i></a> <a href="admin/<?= $data_room['id_room']?>">Voir plus...</a></div></li>
 
     <?php }?>
+ </ul>
  </div>
 </body>
 </html>
